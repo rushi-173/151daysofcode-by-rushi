f304183cb2bdd8137b91fba6c13ec5d1e1f63840
Rushikesh Chaudhari <rushichaudhari1237@gmail.com>
2021-01-15 14:16:46 +0530

    feat: header and footer

--- src/App.js
+++ src/App.js
@@ -13,6 +13,38 @@ function App() {
           <li><a href="">Contact</a></li>
         </ul>
       </nav>
+      <header>
+        <h1>My Journey of #151DaysOfCode</h1>
+        <p>I am sharing my journey of #151DaysOfCode on twitter. I created this webapp to create a collection
+           of all my tweets at one place. So that I can keep counting my progress. </p>
+      </header>
+      <main>
+        
+      </main>
+      <footer>
+      <ul>
+            <li>
+                <a href="https://github.com/rushi-173">
+                    <i class="fab fa-github" aria-hidden="true"></i>
+                </a>
+            </li>
+            <li>
+                <a href="https://rushikeshchaudhari.netlify.app/">
+                    <i class="fas fa-briefcase"></i>
+                </a>
+            </li>
+            <li>
+                <a href="https://twitter.com/rushi_173">
+                    <i class="fab fa-twitter-square"></i>
+                </a>
+            </li>
+            <li>
+                <a href="https://www.linkedin.com/in/rushi173">
+                    <i class="fab fa-linkedin"></i>
+                </a>
+            </li>
+        </ul>
+      </footer>
     </div>
   );
 }
