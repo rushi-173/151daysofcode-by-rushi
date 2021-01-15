cfd6baac4a655a7259f9dbef7a9560955c7704f1
Rushikesh Chaudhari <rushichaudhari1237@gmail.com>
2021-01-15 13:55:52 +0530

    feat: nav

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello</h1>
+      <nav>
+        <div className="logo">
+          <h3>#151DaysOfCode</h3>
+        </div>
+        <ul>
+          <li><a href="">Home</a></li>
+          <li><a href="">Journey</a></li>
+          <li><a href="">Contact</a></li>
+        </ul>
+      </nav>
     </div>
   );
 }
